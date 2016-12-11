4a923aaf32ba86b121c0af78a15f2c34137c3f3d
阿七 <747037402@qq.com>
2016-12-11 23:42:05 +0800

    添加路径接口

---
+++ shine_services/src/main/java/com/shine/constant/Backend.java
@@ -0,0 +1,30 @@
+package com.shine.constant;
+
+/**
+ * Created by hq on 2016/12/11 0011.
+ */
+public interface Backend {
+    interface URL {
+        String BASE = "/backend";
+
+        /**
+         * 主要
+         */
+        interface INDEX {
+            String INDEX = "/index";
+            String INDEX_V5 = "/index_v5";
+        }
+    }
+
+    interface PATH {
+        String BASE = "/backend";
+
+        /**
+         * 主要
+         */
+        interface INDEX {
+            String INDEX = "/index";
+            String INDEX_V5 = "/index_v5";
+        }
+    }
+}
