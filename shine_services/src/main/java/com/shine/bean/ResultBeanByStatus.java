555a5b3c4081bcdad5f7047c557ad2925e0fabd0
7le <silk.heqian@gmail.com>
2017-05-29 00:43:35 +0800

    优化异常处理  以后的异常使用全局的异常处理切面

---
+++ shine_services/src/main/java/com/shine/bean/ResultBeanByStatus.java
@@ -0,0 +1,69 @@
+package com.shine.bean;
+
+/**
+ * 结果bean
+ */
+public class ResultBeanByStatus {
+
+    final public static ResultBeanByStatus SUCCESS = new ResultBeanByStatus(200, "success");
+    final public static ResultBeanByStatus ERROR = new ResultBeanByStatus(400, "error");
+
+    protected int status;
+    protected String msg;
+    protected Object data;
+
+    public ResultBeanByStatus() {
+    }
+
+    public ResultBeanByStatus(int status, String msg) {
+        this(status, msg, null);
+    }
+
+    public ResultBeanByStatus(int status, String msg, Object data) {
+        this.status = status;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    private ResultBeanByStatus copyThis() {
+        return new ResultBeanByStatus(status, msg, null);
+    }
+
+    public static ResultBeanByStatus error(String error) {
+        return new ResultBeanByStatus(ERROR.status, error);
+    }
+
+    public static ResultBeanByStatus success(String error) {
+        return new ResultBeanByStatus(SUCCESS.status, error);
+    }
+
+    public static ResultBeanByStatus success(Object object) {
+        ResultBeanByStatus r = ResultBeanByStatus.SUCCESS.copyThis();
+        r.setData(object);
+        return r;
+    }
+}
